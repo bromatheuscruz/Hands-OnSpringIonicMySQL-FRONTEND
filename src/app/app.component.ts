cb795ef355c81286d67b6b73b402a70272c5101d
Matheus Cruz <bromatheuscruz@gmail.com>
2018-08-31 00:36:53 -0300

    Desabilitar swipe na tela inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = 'HomePage';
+  rootPage: string = 'HomePage';
 
   pages: Array<{title: string, component: string}>;
 
